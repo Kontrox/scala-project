a6c21b0f67ef28ccd7996b56948341eb940bbac7
ASUS-ZENBOOK\Sindre Vingen <sindrevingen@gmail.com>
2017-11-02 23:10:03 +0100

    Finished task 1

--- task1-project/src/main/scala/Bank.scala
+++ task1-project/src/main/scala/Bank.scala
@@ -1,12 +1,29 @@
+import java.util.concurrent.locks.{ReadWriteLock, ReentrantReadWriteLock}
+
+import exceptions.{IllegalAmountException, NoSufficientFundsException}
+
 object Bank {
 
-  private var idCounter: Int = 0
+    private var idCounter: Int = 0
+    private val lock: ReadWriteLock = new ReentrantReadWriteLock()
 
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? // Implement
+    def transaction(from: Account, to: Account, amount: Double): Unit = { // Implement
+        /*if(amount < 0){
+            throw new IllegalAmountException("Invalid transfer. Negative amount.")
+        }
+        else if(from.getBalanceAmount - amount < 0){
+            throw new NoSufficientFundsException("Invalid transfer. Insufficient funds.")
+        }*/
+        from.withdraw(amount)
+        to.deposit(amount)
+    }
 
-  def getUniqueId: Int = {
-    idCounter += 1 // Can this be improved?
-    idCounter
-  }
+    def getUniqueId: Int = {
+        lock.writeLock().lock()
+        try {
+            idCounter += 1
+            idCounter
+        } finally { lock.writeLock().unlock() }
+    }
 
 }
