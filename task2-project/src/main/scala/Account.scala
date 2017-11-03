b1709f1906c6e883f3591420afa9b018761192dd
ASUS-ZENBOOK\Sindre Vingen <sindrevingen@gmail.com>
2017-11-03 12:39:23 +0100

    Began on task 2. Minor changes in task 1

--- task2-project/src/main/scala/Account.scala
+++ task2-project/src/main/scala/Account.scala
@@ -1,3 +1,5 @@
+import java.util.concurrent.locks.{ReadWriteLock, ReentrantReadWriteLock}
+
 import exceptions._
 
 class Account(val bank: Bank, initialBalance: Double) {
@@ -5,13 +7,40 @@ class Account(val bank: Bank, initialBalance: Double) {
   class Balance(var amount: Double) {}
 
   val balance = new Balance(initialBalance)
-  val uid = bank.generateAccountId
+  private val uid: Int = bank.generateAccountId
+  private val lock: ReadWriteLock = new ReentrantReadWriteLock()
 
-  def withdraw(amount: Double): Unit = ??? // Like in part 1
-  def deposit(amount: Double): Unit = ??? // Like in part 1
-  def getBalanceAmount: Double = ??? // Like in part 1
+  def withdraw(amount: Double): Unit = {  // Implement
+    lock.writeLock().lock()
+    try {
+      if(amount < 0){
+        throw new IllegalAmountException("Invalid withdraw. Negative amount.")
+      } else if(balance.amount - amount < 0){
+        throw new NoSufficientFundsException("Invalid withdraw. Insufficient funds.")
+      }
+      balance.amount -= amount
+    } finally { lock.writeLock().unlock() }
+  }
 
-  def transferTo(account: Account, amount: Double) = {
+  def deposit(amount: Double): Unit = { // Implement
+    lock.writeLock().lock()
+    try {
+      if(amount < 0){
+        throw new IllegalAmountException("Invalid deposit. Negative amount.")
+      }
+      balance.amount += amount
+    } finally { lock.writeLock().unlock() }
+  }
+
+  def getBalanceAmount: Double = {
+    lock.writeLock().lock()
+    try balance.amount
+    finally { lock.writeLock().unlock() }
+  }
+
+  def getAccountId: Int = { uid }
+
+  def transferTo(account: Account, amount: Double): Unit = {
     bank addTransactionToQueue (this, account, amount)
   }
 
